bd432d6e1b56833fd02c77492ad5c2531140bae7
Kavin Naik <kavinnaik19@gmail.com>
2025-11-07 15:31:35 +0530

    Add Terraform configuration for AWS resources

---
+++ Terraform/main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allow_ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "linux" {
+  most_recent = true
+  owners      = ["137112412989"] # Amazon Linux
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.linux.id
+  instance_type               = var.instance_type
+  key_name                    = var.ssh_key_name
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  user_data = file("${path.module}/user_data.sh")
+
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
+output "ec2_public_dns" {
+  value = aws_instance.web.public_dns
+}
