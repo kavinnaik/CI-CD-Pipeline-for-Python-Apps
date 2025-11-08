f9422b2231f6fd2f3d73d12a0cccba3848874697
Kavin Naik <kavinnaik19@gmail.com>
2025-11-08 19:24:00 +0530

    Update ssh_key_name variable with type and default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,9 @@
 variable "project_name" { default = "ci-cd-python-aws" }
 variable "aws_region"   { type = string }
 variable "instance_type" { default = "t3.micro" }
-variable "ssh_key_name"  { description = "Existing AWS key pair name" }
+variable "ssh_key_name" {
+  description = "Name of the SSH key pair to use for EC2"
+  type        = string
+  default     = "ci-cd-key"
+}
 variable "allow_ssh_cidr" { default = "0.0.0.0/0" } # replace with your IP/CIDR for security
